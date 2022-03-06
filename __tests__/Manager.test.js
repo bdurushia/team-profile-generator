8ff9999c850c45307964cf85b63ccd070514d06d
bdurushia <b.durushia@gmail.com>
2022-03-06 00:56:39 -0600

    Wrote tests for Manager class, all tests passing.

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require('../lib/Manager');
+
+test('Check if a Manager object is created', () => {
+    const manager = new Manager('Philip', '35', 'phil.boss@iamtheboss.com');
+
+    expect(manager).toEqual(expect.any(Object));
+});
+
+test('Check if parameters are being used properly', () => {
+    const manager = new Manager('Philip', '35', 'phil.boss@iamtheboss.com');
+
+    expect(manager.name).toBe('Philip');
+    expect(manager.id).toBe('35');
+    expect(manager.email).toBe('phil.boss@iamtheboss.com');
+});
+
+test('Gets the role of the Manager', () => {
+    const manager = new Manager('Philip', '35', 'phil.boss@iamtheboss.com');
+
+    expect(manager.getRole()).toBe('Manager');
+});
