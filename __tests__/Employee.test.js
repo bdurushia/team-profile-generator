14cbf5390fee1d9fd9fb7d1c85281bd715c805c3
bdurushia <b.durushia@gmail.com>
2022-03-06 00:36:18 -0600

    Wrote tests for Employee object, all tests passed.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,39 @@
-// const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee');
 
-// test('Creates an Employee Object', () => {
-//     const employee = new Employee('James', '23', 'james@gmail.com')
+test('Creates an Employee Object', () => {
+    const employee = new Employee('James', '23', 'james@gmail.com');
 
-//     expect(employee).toEqual(expect.any(Object));
-//     // expect(employee.name).toBe('James');
-//     // expect(employee.id).toBe('23');
-//     // expect(employee.email).toBe('james@gmail.com');
-// });
+    expect(employee).toEqual(expect.any(Object));
+});
+
+test('Checks to see if the Employee parameters are being used properly', () => {
+    const employee = new Employee('James', '23', 'james@gmail.com');
+
+    expect(employee.name).toBe('James');
+    expect(employee.id).toBe('23');
+    expect(employee.email).toBe('james@gmail.com');
+})
+
+test('Gets the name of the Employee', () => {
+    const employee = new Employee('James', '23', 'james@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+});
+
+test('Gets the ID of an Employee', () => {
+    const employee = new Employee('James', '23', 'james@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+})
+
+test('Gets the email address of an Employee', () => {
+    const employee = new Employee('James', '23', 'james@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+})
+
+test('Gets the role of the Employee', () => {
+    const employee = new Employee('James', '23', 'james@gmail.com');
+
+    expect(employee.getRole()).toBe('Employee');
+})
