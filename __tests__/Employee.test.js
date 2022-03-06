fe225dec9d29c0a7847b71111f6147588439a339
bdurushia <b.durushia@gmail.com>
2022-03-06 00:11:36 -0600

    Built class object constructors for Employee, Manager, Engineer, and Intern. Employee being the parent class.

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+// const Employee = require('../lib/Employee');
+
+// test('Creates an Employee Object', () => {
+//     const employee = new Employee('James', '23', 'james@gmail.com')
+
+//     expect(employee).toEqual(expect.any(Object));
+//     // expect(employee.name).toBe('James');
+//     // expect(employee.id).toBe('23');
+//     // expect(employee.email).toBe('james@gmail.com');
+// });
